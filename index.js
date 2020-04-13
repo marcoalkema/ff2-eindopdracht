70bd076439c97635d2b03fc92d55f5f668d2dd30
rebal67 <45282854+Rebal67@users.noreply.github.com>
2020-04-13 21:58:52 +0200

    heb jouw functies gebruikt sorry :P

--- index.js
+++ index.js
@@ -1,25 +1,80 @@
 const items = [
+
   {
-    class: "foo",
-    onClick: 'greeting()',
+    class: "i1",
+    onClick: `toggleClass('.i2')`,
+    color: "red"
+  },
+  {
+    class: "i2",
+    onClick: `toggleClass('.i1')`,
     color: "green"
   },
   {
-    class: "bar",
-    onClick: `setColor('.foo')`,
-    color: "red"
-  }
+    class: "i3",
+    onClick: '',
+    color: "blue"
+  },
+  {
+    class: "i4",
+    onClick: `toggleClass('main')`,
+    color: "yellow"
+  },
+  {
+    class: "i5",
+    onClick: `toggleClass('.i4')`,
+    color: "silver"
+  },
+  {
+    class: "i6",
+    onClick: `rotateGrid()`,
+    color: "orange"
+  },
+  {
+    class: "i7",
+    onClick: `randomColor(event)`,
+    color: "gold"
+  },
+  {
+    class: "i8",
+    onClick: `reloade()`,
+    color: "black"
+  },
+  {
+    class: "i9",
+    onClick: `newTab('https://www.youtube.com/watch?v=19pp2jNapL4')`,
+    color: "pink"
+  },
+  
 ]
 const mainHtml = items.reduce(mkHtmlStr, '');
 
-function setColor (query) {
-  document.querySelector(`${query}`).classList.toggle('color');
+function toggleClass (query) {
+  document.querySelector(`${query}`).classList.toggle('active');
 }
 
-function greeting () {
-  alert('Hello!');
+function rotateGrid() {
+  document.querySelector('main').classList.toggle('rotate')
 }
 
+function randomColor(e){
+  // console.log('test')
+  let colors = ['red','green','blue','yellow','orange'];
+  let div = e.target;
+  setInterval(()=>{
+    let random = Math.floor(Math.random() * colors.length);
+
+    div.style.backgroundColor = colors[random];
+  },1000);
+}
+
+function reloade() {
+  window.location.reload();
+}
+
+function newTab(url){
+  window.open(url, '_blank');
+}
 function mkHtmlStr (htmlStr, item) {
   const stringToAppend = `<div onclick="${item.onClick}"
                                class="${item.class}"
@@ -28,5 +83,6 @@ function mkHtmlStr (htmlStr, item) {
 
   return htmlStr + stringToAppend;
 }
+
 
 document.querySelector('main').innerHTML = mainHtml;
